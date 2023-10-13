2c3512ce8e56ee246d9c0af091a44086290e3bad
edarchimbaud <edouard.darchimbaud@gmail.com>
2023-10-13 22:00:11 +0200

    Entrypoint for execution

--- examples/veighna_trader/run_execution.py
+++ examples/veighna_trader/run_execution.py
@@ -2,9 +2,8 @@ from vnpy.event import EventEngine
 from vnpy.trader.engine import MainEngine
 from vnpy.trader.ui import MainWindow, create_qapp
 
-# from vnpy_ib import IBGateway
-from vnpy_ctastrategy import CtaStrategyApp
-from vnpy_ctabacktester import CtaBacktesterApp
+from vnpy_algotrading import AlgoTradingApp
+from vnpy_ib import IbGateway
 
 
 def main():
@@ -12,9 +11,8 @@ def main():
     qapp = create_qapp()
     event_engine = EventEngine()
     main_engine = MainEngine(event_engine)
-    # main_engine.add_gateway(IBGateway)
-    main_engine.add_app(CtaStrategyApp)
-    main_engine.add_app(CtaBacktesterApp)
+    main_engine.add_app(AlgoTradingApp)
+    main_engine.add_gateway(IbGateway)
     main_window = MainWindow(main_engine, event_engine)
     main_window.showMaximized()
     qapp.exec()
